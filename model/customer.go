1c3c89929e70de2c7d5f74781800016160155855
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-07-07 13:59:47 +0700

    add server.go

--- model/customer.go
+++ model/customer.go
@@ -2,5 +2,5 @@ package model
 
 type Customer struct {
 	LopeiId int32
-	Balance int32
+	Balance float32
 }
---
+++ service/server.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"golang_lopei_grpc_server/repository"
+)
+
+type LopeiService interface{
+	CheckBalanceMessage(ctx context.Context, in *CheckBalanceMessage)(*ResultMessage, error)
+
+	DoPayment(ctx context.Context, in *PaymentMessage)(*ResultMessage, error)
+}
+
+type lopeiService struct {
+	repo repository.LopeiRepository
+}
+
+func (c *lopeiService) CheckBalanceMessage(ctx context.Context, in *CheckBalanceMessage) (*ResultMessage, error) {
+	lopeId := in.LopeiId
+	customer, err := c.repo.RetrieveById(lopeId)
+
+	if err != nil {
+		return nil, err 
+	}
+
+	l, err := json.Marshal(customer) 
+	if err != nil {
+		return nil, err 
+	}
+	ResultMessage := &ResultMessage{
+		Result: string(l),
+		Eror: nil,
+	}
+	return ResultMessage, nil 
+}
+
+func (l *lopeiService) DoPayment(ctx context.Context, in *PaymentMessage)(*ResultMessage, error){
+	lopeId := in.LopeId
+	amount := in.Amount
+
+	customer, err := l.repo.RetrieveById(lopeId)
+	if err != nil {
+		return nil, err 
+	}
+
+	if customer.Balance < amount {
+		return &ResultMessage{
+			Result: "FAILED",
+			Eror: &Error{
+				Code: "X07",
+				Message: "Insufficient Balance",
+			},
+		}, nil 
+	}
+
+	resultMessage := &ResultMessage{
+		Result: "SUCCESS",
+		Eror: nil,
+	}
+	return resultMessage, nil 
+}
+
+
+
+func NewLopeiService(repo repository.LopeiRepository) LopeiService {
+	service := new(lopeiService)
+	service.repo = repo 
+	return service 
+}
