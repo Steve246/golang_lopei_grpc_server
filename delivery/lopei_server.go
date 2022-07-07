c25668a6f2fb0af3d3af79d398cf198d3f63128b
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-07-07 14:50:24 +0700

    .

--- delivery/lopei_server.go
+++ delivery/lopei_server.go
@@ -1,8 +1,10 @@
 package delivery
 
 import (
+	"golang_lopei_grpc_server/config"
 	"golang_lopei_grpc_server/manager"
 	"golang_lopei_grpc_server/service"
+	"log"
 	"net"
 
 	"google.golang.org/grpc"
@@ -17,4 +19,31 @@ type LopeiGrpcServer struct {
 
 func(lgs *LopeiGrpcServer) Run() {
 	service.RegisterLopeiPaymentServer(lgs.server, lgs.serviceManager.LopeiService())
+
+	log.Println("Server Run", lgs.netListen.Addr().String())
+
+	err := lgs.server.Serve(lgs.netListen)
+
+	if err != nil {
+		log.Fatalln("Failed to Serve..", err)
+	}
+}
+
+
+func Server() *LopeiGrpcServer{
+	lopeiGrpcServer := new(LopeiGrpcServer)
+	c := config.NewConfig()
+
+	listen, err := net.Listen("tcp", c.Url)
+	if err != nil {
+		log.Fatalln("Failed to Listen", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	repoManager := manager.NewRepositoryManager()
+	lopeiGrpcServer.serviceManager = manager.NewServiceManager(repoManager)
+
+	lopeiGrpcServer.netListen = listen 
+	lopeiGrpcServer.server = grpcServer
+	return lopeiGrpcServer
 }
